f03cc84dd4f22d924f97b3c781a818256710fc5c
Arvid Tomayko-Peters <arvidtp@gmail.com>
2013-11-07 16:09:34 -0500

    atp.hex2int: pass decimal and float numbers through (only try to convert strings), clarify behavior in comments

--- js/atp.hex2int.js
+++ js/atp.hex2int.js
@@ -1,5 +1,9 @@
-//preface hex numbers with "#" to make them work.
-//Can send in a list a long as all members have a #
+//preface hex numbers with "#" to make them work 
+//(but it will attempt to read all symbols as hex)
+//I didn't use "0x" as prefix because max changes those back to 
+//decimal automatically in message box arguments, etc.
+//You can send in a list a long as all hex members are prefaced with "#"
+//ie "#3F #7e #23…"
 
 function anything()
 {
@@ -7,8 +11,12 @@ function anything()
 	var i;
 	var a = arrayfromargs(messagename, arguments);
 	for(i=0;i<a.length;i++) {
-		a[i] = a[i].replace("#","0x");
-		v[i] = parseInt(a[i],16);
+		if (typeof a[i] == 'string') { //if string
+			a[i] = a[i].replace("#","0x");
+			v[i] = parseInt(a[i],16);
+		} else { //if not (ie an int), just pass it through
+			v[i] = a[i];
+		}
 	}
 	outlet(0,v);
 }
