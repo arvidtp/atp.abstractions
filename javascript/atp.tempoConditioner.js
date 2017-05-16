2b43cd90c14eaf2e727e2a9bbfe4e5a8aa94bba9
Arvid Tomayko-Peters <arvidtp@gmail.com>
2017-05-16 19:13:12 -0400

    fix something (don’t remember what)

--- javascript/atp.tempoConditioner.js
+++ javascript/atp.tempoConditioner.js
@@ -1,16 +1,17 @@
-var minTime = 16.
-var maxTime = 40.
+var minTime = 13.0;
+var maxTime = 40.0;
 
 function msg_float (a) {
 	if (a <= 0) {
 		return;
+	} else if ((a > maxTime) || ( a < minTime)) {
+		while (a > maxTime) {
+			a *= 0.5;
+		}
+		while (a < minTime) {
+			a *= 2.0;
+		}
 	}
-	while (a > maxTime) {
-		a *= 0.5;
-	}
-	while (a < minTime) {
-		a *= 2.0;
-	}	
 	outlet(0, a);
 }
 
